a673da80299e5b60205ce2f8eb93425da9af6330
robert2387 <robert.ionut.neagu@gmail.com>
2025-11-06 15:07:45 +0100

    added main

--- dev-aws-terraform/providers.tf
+++ dev-aws-terraform/providers.tf
@@ -1,7 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
+}
+
+provider "aws" {
+  region                   = "eu-north-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "aws-credentials"
 }
